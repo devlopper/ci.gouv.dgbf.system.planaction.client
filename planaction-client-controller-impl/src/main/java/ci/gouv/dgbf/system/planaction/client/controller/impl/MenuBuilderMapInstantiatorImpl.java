e1f46f2b34e1877ee7b3782aad9db241d33f5521
Christian Yao Komenan <kycdev@gmail.com>
2020-02-23 12:43:49 +0000

    package updated

---
+++ planaction-client-controller-impl/src/main/java/ci/gouv/dgbf/system/planaction/client/controller/impl/MenuBuilderMapInstantiatorImpl.java
@@ -0,0 +1,9 @@
+package ci.gouv.dgbf.system.planaction.client.controller.impl;
+
+import java.io.Serializable;
+
+@ci.gouv.dgbf.system.planaction.server.annotation.System
+public class MenuBuilderMapInstantiatorImpl extends org.cyk.utility.client.controller.component.menu.AbstractMenuBuilderMapInstantiatorImpl implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ planaction-client-deployment/script/docker/push.bat
@@ -0,0 +1,2 @@
+docker tag planaction-client DESTINATION_HOST/planaction-client
+docker push DESTINATION_HOST/planaction-client
