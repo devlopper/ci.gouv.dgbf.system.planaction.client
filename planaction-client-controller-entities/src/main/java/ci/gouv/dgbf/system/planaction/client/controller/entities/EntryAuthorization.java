ad7b8837262e512dfbd0713aa28a16f12bfb3a2e
Christian Yao Komenan <kycdev@gmail.com>
2020-03-25 15:32:47 +0000

    commit

--- planaction-client-controller-entities/src/main/java/ci/gouv/dgbf/system/planaction/client/controller/entities/EntryAuthorization.java
+++ planaction-client-controller-entities/src/main/java/ci/gouv/dgbf/system/planaction/client/controller/entities/EntryAuthorization.java
@@ -1,7 +1,10 @@
 package ci.gouv.dgbf.system.planaction.client.controller.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+
+import org.cyk.utility.__kernel__.collection.CollectionHelper;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,27 @@ public class EntryAuthorization extends AbstractAmountPlanning implements Serial
 	private Imputation imputation;
 	private Collection<PaymentCredit> paymentCredits;
 	private Long paymentCreditsAmountsCumulation;
+	
+	public Collection<PaymentCredit> getPaymentCredits(Boolean injectIfNull) {
+		if(paymentCredits == null && Boolean.TRUE.equals(injectIfNull))
+			paymentCredits = new ArrayList<>();
+		return paymentCredits;
+	}
+	
+	public PaymentCredit getPaymentCreditByYear(Short year) {
+		if(CollectionHelper.isEmpty(paymentCredits) || year == null)
+			return null;
+		for(PaymentCredit paymentCredit : paymentCredits)
+			if(paymentCredit.getYear() != null && paymentCredit.getYear().equals(year))
+				return paymentCredit;
+		return null;
+	}
+	
+	public PaymentCredit getPaymentCreditByYearIndex(Byte yearIndex) {
+		if(yearIndex == null || imputation == null)
+			return null;
+		return getPaymentCreditByYear((short) (imputation.getActionPlan().getYear()+yearIndex));
+	}
 	
 	@Override
 	public String toString() {
--- planaction-client-controller-impl/src/main/java/ci/gouv/dgbf/system/planaction/client/controller/impl/ImputationFinancialProgrammingPage.java
+++ planaction-client-controller-impl/src/main/java/ci/gouv/dgbf/system/planaction/client/controller/impl/ImputationFinancialProgrammingPage.java
@@ -1,7 +1,9 @@
 package ci.gouv.dgbf.system.planaction.client.controller.impl;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -13,10 +15,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.cyk.utility.__kernel__.collection.CollectionHelper;
 import org.cyk.utility.__kernel__.constant.ConstantEmpty;
 import org.cyk.utility.__kernel__.data.structure.grid.Grid;
+import org.cyk.utility.__kernel__.data.structure.grid.Grid.Row;
 import org.cyk.utility.__kernel__.identifier.resource.ParameterName;
 import org.cyk.utility.__kernel__.map.MapHelper;
 import org.cyk.utility.__kernel__.number.NumberHelper;
 import org.cyk.utility.__kernel__.properties.Properties;
+import org.cyk.utility.__kernel__.throwable.RuntimeException;
 import org.cyk.utility.__kernel__.user.interface_.message.MessageRenderer;
 import org.cyk.utility.client.controller.web.WebController;
 import org.cyk.utility.client.controller.web.jsf.primefaces.model.collection.AbstractCollection;
@@ -56,22 +60,56 @@ public class ImputationFinancialProgrammingPage extends AbstractPageContainerMan
 				columnsKeys.add("paymentCredit"+index);		
 			Grid grid = new Grid().setListener(new Grid.Listener.AbstractImpl() {
 				@Override
-				public Object listenGetColumnValueFromRowObject(Grid grid, Object rowObject, Object columnKey) {
-					if(columnKey!=null && StringUtils.startsWith(columnKey.toString(), "paymentCredit")) {
-						Integer index = NumberHelper.getInteger(StringUtils.substringAfter(columnKey.toString(), "paymentCredit"));
-						PaymentCredit paymentCredit = CollectionHelper.getElementAt(((EntryAuthorization)rowObject).getPaymentCredits(), index);
-						if(paymentCredit == null)
-							return null;
+				public Object getColumnValueFromRowObject(Grid grid, Object rowObject, Object columnKey) {
+					if(grid == null || rowObject == null || columnKey == null)
+						return null;
+					EntryAuthorization entryAuthorization = (EntryAuthorization)rowObject;
+					if(columnKey.toString().startsWith(PAYMENT_CREDIT)) {
+						Byte paymentCreditYearIndex = getPaymentCreditColumnIndex(columnKey);
+						PaymentCredit paymentCredit = ((EntryAuthorization)rowObject).getPaymentCreditByYearIndex(paymentCreditYearIndex);
+						if(paymentCredit == null) {
+							paymentCredit = new PaymentCredit();
+							paymentCredit.setEntryAuthorization(entryAuthorization);
+							paymentCredit.setYear((short) (entryAuthorization.getImputation().getActionPlan().getYear()+paymentCreditYearIndex));
+							paymentCredit.setAmount(0l);
+							entryAuthorization.getPaymentCredits(Boolean.TRUE).add(paymentCredit);
+						}
 						return paymentCredit.getAmount();
 					}
-					return super.listenGetColumnValueFromRowObject(grid, rowObject, columnKey);
+					return super.getColumnValueFromRowObject(grid, rowObject, columnKey);
+				}
+				
+				@Override
+				public void setColumnValueToRowObject(Grid grid, Row row, Object columnKey, Object value) {
+					if(grid == null || row == null || columnKey == null)
+						return;
+					if(EntryAuthorization.FIELD_YEAR.equals(columnKey) || EntryAuthorization.FIELD_AMOUNT.equals(columnKey))
+						super.setColumnValueToRowObject(grid, row, columnKey, value);
+					else {
+						EntryAuthorization entryAuthorization = (EntryAuthorization)row.getObject();
+						Byte paymentCreditYearIndex = getPaymentCreditColumnIndex(columnKey);
+						PaymentCredit paymentCredit = entryAuthorization.getPaymentCreditByYearIndex(paymentCreditYearIndex);
+						if(paymentCredit == null)
+							throw new RuntimeException("No payment credit found at "+columnKey+" for "+entryAuthorization);						
+						paymentCredit.setAmount((Long) value);												
+					}					
 				}
 				
 				@Override
 				public Object formatNextColumnKey(Grid grid) {
-					return "paymentCredit"+(grid.getNumberOfColumns() - 2);
+					return PAYMENT_CREDIT+(grid.getNumberOfColumns() - 2);
 				}
-			}).addColumnsKeys(columnsKeys).setColumnKeyFormat("paymentCredit%s").addRows(imputation.getEntryAuthorizations());
+				
+				@Override
+				protected Object formatValue(Grid grid, Row row, Object columnKey, Object value) {
+					if(value == null)
+						return null;
+					if(EntryAuthorization.FIELD_YEAR.equals(columnKey))
+						return value.toString();
+					return NumberFormat.getInstance(Locale.FRENCH).format(value);
+				}
+				
+			}).addColumnsKeys(columnsKeys).setColumnKeyFormat(PAYMENT_CREDIT_FORMAT).addRows(imputation.getEntryAuthorizations());
 			
 			dataTable = DataTable.build(DataTable.FIELD_ELEMENT_CLASS,Grid.Row.class,DataTable.FIELD_DATA_GRID,grid,DataTable.FIELD_EDITABLE,Boolean.TRUE
 					,DataTable.FIELD_LISTENER,new AbstractDataTable.Listener.AbstractImpl() {
@@ -87,8 +125,8 @@ public class ImputationFinancialProgrammingPage extends AbstractPageContainerMan
 					}else if(EntryAuthorization.FIELD_AMOUNT.equals(fieldName)) {
 						map.put(Column.FIELD_HEADER_TEXT, "Montant");
 						map.remove(Column.FIELD_REMOVE_COMMAND_BUTTON);
-					}else if(StringUtils.startsWith(fieldName, "paymentCredit")) {
-						Integer index = NumberHelper.getInteger(StringUtils.substringAfter(fieldName, "paymentCredit"));
+					}else if(StringUtils.startsWith(fieldName, PAYMENT_CREDIT)) {
+						Byte index = getPaymentCreditColumnIndex(fieldName);
 						map.put(Column.FIELD_HEADER_TEXT, "C.P. "+(imputation.getActionPlan().getYear()+index));
 						if(index <= 2)
 							map.remove(Column.FIELD_REMOVE_COMMAND_BUTTON);
@@ -97,11 +135,21 @@ public class ImputationFinancialProgrammingPage extends AbstractPageContainerMan
 				}
 				
 				@Override
+				public String listenGetStyleClassByRecordByColumn(Object record, Integer recordIndex,Column column, Integer columnIndex) {
+					if(record == null || recordIndex == null || column == null || columnIndex == null)
+						return null;
+					if(EntryAuthorization.FIELD_YEAR.equals(column.getFieldName()))
+						return "cyk-font-weight-bold";
+					return "cyk-text-align-right";					
+				}
+				
+				@Override
 				public Object listenSave(AbstractCollection collection) {
 					AbstractDataTable dataTable = (AbstractDataTable) collection;
 					MessageRenderer.getInstance().render(dataTable.getDataGrid().toString());
 					dataTable.getDataGrid().writeValuesToObjects();
-					MessageRenderer.getInstance().render(dataTable.getDataGrid().getRows().stream().map(row -> row.getObject()).collect(Collectors.toList()).toString());
+					MessageRenderer.getInstance().render(dataTable.getDataGrid().getRows().stream()
+							.map(row -> ((EntryAuthorization)row.getObject()).getPaymentCredits() ).collect(Collectors.toList()).toString());
 					return null;
 				}
 			
@@ -115,6 +163,15 @@ public class ImputationFinancialProgrammingPage extends AbstractPageContainerMan
 		
 	@Override
 	protected String __getWindowTitleValue__() {
-		return "Programmation financière"+(imputation == null ? ConstantEmpty.STRING : " de l'imputation "+imputation);
+		return "Programmation financière"+(imputation == null ? ConstantEmpty.STRING : " : "+imputation);
 	}
+	
+	private static Byte getPaymentCreditColumnIndex(Object columnKey) {
+		return NumberHelper.get(Byte.class,StringUtils.substringAfter(columnKey.toString(), PAYMENT_CREDIT));
+	}
+	
+	/**/
+	
+	private static final String PAYMENT_CREDIT = "paymentCredit";
+	private static final String PAYMENT_CREDIT_FORMAT = PAYMENT_CREDIT+"%s";
 }
